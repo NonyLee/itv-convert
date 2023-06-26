0d969950e67e563df1f80094a658cfd2974c02d4
Lee Nony <xiuyu.lee@163.com>
2023-06-26 21:15:38 +0800

    feat - 保持顺序

---
+++ README.md
@@ -0,0 +1,5 @@
+## Excel转m3u8
+
+源码参考：https://github.com/KyonLi/ottcn2m3u8
+
+Excel来源：https://github.com/SPX372928/MyIPTV
--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
+	"log"
+	"os"
+	"flag"
+	"io"
 
 	"github.com/xuri/excelize/v2"
+)
+
+var (
+	outputFile = flag.String("o", "channel.m3u8", "Output file path.")
 )
 
 type Channel struct {
@@ -16,6 +24,10 @@ type Channel struct {
 	category     string
 	channelId    string
 	url          string
+}
+
+func (c *Channel) toString() string {
+	return fmt.Sprintf("#EXTINF:-1,%s\n%s\n", c.name, c.url)
 }
 
 func filterBitRate(cs []Channel) []Channel {
@@ -47,6 +59,40 @@ func selectChannel(cs []Channel) Channel {
 	return cs[0]
 }
 
+func generateM3U8(channels *[]Channel) {
+	fmt.Println("Generating m3u8...")
+
+	content := fmt.Sprintf("#EXTM3U\n\n")
+	for _, c := range *channels {
+		content += c.toString()
+	}
+
+	f, err := os.Create(*outputFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	_, err = io.WriteString(f, content)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Done, all saved to %s\n", *outputFile)
+}
+
+func unique(s []string) []string {
+    inResult := make(map[string]bool)
+    var result []string
+    for _, str := range s {
+        if _, ok := inResult[str]; !ok {
+            inResult[str] = true
+            result = append(result, str)
+        }
+    }
+    return result
+}
+
 func main() {
 	f, err := excelize.OpenFile("itv.xlsx")
 	if err != nil {
@@ -66,6 +112,7 @@ func main() {
 		return
 	}
 	m := make(map[string][]Channel)
+	cnames := make([]string, 0)
 	for i, row := range rows {
 		if i == 0 || row[3] == "湖南移动" || row[3] == "宁夏移动" || row[3] == "咪咕视频" {
 			continue
@@ -88,12 +135,25 @@ func main() {
 		}
 
 		m[c.name] = append(m[c.name], c)
+
+		cnames = append(cnames, c.name)
 	}
 
+	cnames = unique(cnames)
+
 	channels := make([]Channel, 0)
-	for _, v := range m {
+	// for _, v := range m {
+	// 	hcs := filterBitRate(v)
+	// 	c := selectChannel(hcs)
+	// 	channels = append(channels, c)
+	// }
+	for _, n := range cnames {
+		v := m[n]
 		hcs := filterBitRate(v)
 		c := selectChannel(hcs)
 		channels = append(channels, c)
+		fmt.Println(c)
 	}
+
+	generateM3U8(&channels)
 }
